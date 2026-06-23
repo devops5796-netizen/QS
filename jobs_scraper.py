954406ce6a6e229b002ac241da288eaa188b930e
wwo115570-commits <wwo115570@gmail.com>
2026-06-23 11:09:49 +0300

    jobs in parallel

--- jobs_scraper.py
+++ jobs_scraper.py
@@ -357,7 +357,7 @@ if __name__ == "__main__":
     start = int(sys.argv[1]) if len(sys.argv) > 1 else 0
     end   = int(sys.argv[2]) if len(sys.argv) > 2 else None
     run(
-        output_excel=f"jobs.xlsx",
+        output_excel=f"jobs_{start}_{end}.xlsx",
         start_page=start,
         end_page=end
     )
